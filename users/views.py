fe3b31d5956160c4639e4ddc8fbf303b40b12f13
wj <wj@wjui-MacBookPro.local>
2022-07-21 16:30:43 +0900

    MODIFY : signup login views.py

--- users/views.py
+++ users/views.py
@@ -18,7 +18,7 @@ class SignUpView(View):
             phone_number = data['phone_number']
             birthdate	 = data['birthdate']
             
-            REGEX_EMAIL		 = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+            REGEX_EMAIL	  	 = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
             REGEX_PASSWORD	 = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
             REGEX_PHONE_NUMBER	 = '^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$'
             REGEX_BIRTHDATE	 = '^(19[0-9][0-9]|20\d{2})-(0[0-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$'
@@ -41,12 +41,12 @@ class SignUpView(View):
             hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
             
             user = User(
-                email		 = email,
-                password	 = hashed_password,
-                first_name	 = first_name,
-                last_name	 = last_name,
-                phone_number	 = phone_number,
-                birthdate	 = birthdate
+                email        = email,
+                password     = hashed_password,
+                first_name   = first_name,
+                last_name    = last_name,
+                phone_number = phone_number,
+                birthdate    = birthdate
             )
             
             user.save()
@@ -61,22 +61,16 @@ class LoginView(View):
         try:
             data = json.loads(request.body)
             
-            email	 = data['email']
-            password	 = data['password']
+            email    = data['email']
+            password = data['password']
             
-            REGEX_EMAIL		 = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-            REGEX_PASSWORD	 = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
+            user = User.objects.get(email=email)
+
+            if not User.objects.filter(email=email).exists():
+                return JsonResponse({"message" : "INVALID_USER"}, status=401)
             
-            if not re.match(REGEX_EMAIL, email):
-                return JsonResponse({'message':'INVALID_EMAIL'}, status=400)
-            
-            if not re.match(REGEX_PASSWORD, password):
-                return JsonResponse({'message':'INVALID_PASSWORD'}, status=400)
-            
-            user = User.objects.get(email = email)
-            
-            if not bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
-                return JsonResponse({'message':'INVALID_PASSWORD'}, status=401)
+            if not bcrypt.checkpw(password.encode('utf-8'), User.objects.get(email=email).password.encode("utf-8")):
+                return JsonResponse({"message" : "INVALID_USER"}, status=401)
             
             access_token = jwt.encode({'id':user.id}, settings.SECRET_KEY, settings.ALGORITHM)
             
@@ -84,6 +78,3 @@ class LoginView(View):
         
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR'}, status=400)
-        
-        except User.DoesNotExist:
-            return JsonResponse({'message':'INVALID_EMAIL'}, status=401)
