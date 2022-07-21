fa380bf476f05792d4ff51d2adafc96cca5d9193
melony <hjz1010@gmail.com>
2022-07-21 15:07:27 +0900

    ADD: 컨벤션 정리

--- products/views.py
+++ products/views.py
@@ -3,7 +3,7 @@ import json
 from django.http import JsonResponse
 from django.views import View
 
-from .models import Category, Furniture, SubCategory, Product
+from .models import Category, Furniture, ProductImage, SubCategory, Product, Brand, Color 
 
 
 class ListView(View):
@@ -18,13 +18,12 @@ class ListView(View):
             sub_categories  = SubCategory.objects.all()
 
             if selected_category in [category.name for category in main_categories]:
-                subs = SubCategory.objects.filter(
-                    category__name=selected_category)
+                subs = SubCategory.objects.filter(category__name=selected_category)
+                
                 sub_list = []
                 for sub in subs:
                     sub_list.append(sub.name)
-                products = Product.objects.filter(
-                    sub_category__category__name=selected_category)
+                products = Product.objects.filter(sub_category__category__name=selected_category)
 
                 product_list = []
                 for product in products:
@@ -35,10 +34,12 @@ class ListView(View):
                         'productName': product.furniture.name + '_' + product.color.name,
                         'price'      : product.price
                     })
+
                 return JsonResponse({'message': 'SUCCESS', 'sub_list': sub_list, 'product_list': product_list}, status=200)
+
             elif selected_category in [category.name for category in sub_categories]:
-                products = Product.objects.filter(
-                    sub_category__name=selected_category)
+                products = Product.objects.filter(sub_category__name=selected_category)
+
                 product_list = []
                 for product in products:
                     product_list.append({
@@ -49,6 +50,7 @@ class ListView(View):
                         'price'      : product.price
                     })
                 return JsonResponse({'message': 'SUCCESS', 'product_list': product_list}, status=200)
+                
             else:
                 return JsonResponse({'message': 'INVALID_CATEGORY'}, status=400)
         except KeyError:
