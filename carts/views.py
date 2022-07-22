8f2f407f6b392c84d1ac79e7f3f618af0bd0eccb
enddl3224 <enddl3224@naver.com>
2022-07-22 20:25:15 +0900

    ADD: core App생성, carts App생성

--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http     import JsonResponse
+from django.views    import View
+
+from products.models import Product
+from carts.models    import Cart
+from core.utils      import LoginConfirm
+
+class CartView(View):
+
+    @LoginConfirm
+    def post(self, request):
+        try:
+            data     = json.loads(request.body)
+
+            product  = Product.objects.get(id = data['product_id'])
+            user     = request.user
+            quantity = data['quantity']
+
+            Cart.objects.create(
+                user     = user,    
+                product  = product, 
+                quantity = quantity 
+            )
+            return JsonResponse({"message" : "Created"}, status = 200)
+
+        except KeyError:
+            return JsonResponse({"message" : "KeyError"}, status = 400)
+        
+        except Product.DoesNotExist:
+            return JsonResponse({"message" : "INVALID_PRODUCT"}, status = 400)
+
+        except json.JSONDecodeError:
+            return JsonResponse({"message" : "JsonError"}, status = 400)
+
+    @LoginConfirm
+    def get(self, request):
+        carts = Cart.objects.filter(user_id = request.user.id)
+        results_cart = []
+
+        for cart in carts:
+            results_cart.append(
+                {
+                    "cart_id"          : cart.id,
+                    "cart_image"       : cart.product.thumbnail_image_url,
+                    "user_firstname"   : cart.user.first_name,
+                    "user_lastname"    : cart.user.last_name,
+                    "product_furniture": cart.product.furniture.name,
+                    "product_brand"    : cart.product.furniture.brand.name,
+                    "product_color"    : cart.product.color.name,
+                    "price"            : cart.product.price,
+                    "quantity"         : cart.quantity,
+                }
+            )
+        return JsonResponse({"carts" : results_cart}, status = 200)
