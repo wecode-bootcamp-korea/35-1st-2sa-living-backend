4bbc4d9c9f4a7cc7378c5c7d2c764f7ec8368125
enddl3224 <enddl3224@naver.com>
2022-07-24 14:19:20 +0900

    MODIFY: carts APP - views.py 수정

--- carts/views.py
+++ carts/views.py
@@ -12,22 +12,26 @@ class CartView(View):
     @LoginConfirm
     def post(self, request):
         try:
-            data     = json.loads(request.body)
-
-            product  = Product.objects.get(id = data['product_id'])
-            user     = request.user
+            data      = json.loads(request.body)
+            product   = Product.objects.get(id = data['product_id'])
+            user      = request.user
             quantity = data['quantity']
 
-            Cart.objects.create(
-                user     = user,    
-                product  = product, 
-                quantity = quantity 
-            )
+            if not Cart.objects.filter(product=product).exists():
+                Cart.objects.create(
+                user     = user,
+                product  = product,
+                quantity = quantity
+                )
+            else:
+                temp   = Cart.objects.get(product=product).quantity
+                temp += quantity
+                Cart.objects.filter(product=product).update(quantity=temp)
             return JsonResponse({"message" : "Created"}, status = 200)
 
         except KeyError:
             return JsonResponse({"message" : "KeyError"}, status = 400)
-        
+
         except Product.DoesNotExist:
             return JsonResponse({"message" : "INVALID_PRODUCT"}, status = 400)
 
@@ -56,3 +60,30 @@ class CartView(View):
                 }
             )
         return JsonResponse({"carts" : results_cart}, status = 200)
+
+    @LoginConfirm
+    def delete(self, request, cart_id):
+        if Cart.objects.filter(user=request.user, id=cart_id).delete()[0]:
+
+            carts = Cart.objects.filter(user_id = request.user.id)
+            results_cart = []
+
+            for cart in carts:
+                results_cart.append(
+                    {
+                        "cart_id"                     : cart.id,
+                        "cart_image"                  : cart.product.thumbnail_image_url,
+                        "user_firstname"              : cart.user.first_name,
+                        "user_lastname"               : cart.user.last_name,
+                        "furniture_korean_name"       : cart.product.furniture.korean_name,
+                        "furniture_english_name"      : cart.product.furniture.english_name,
+                        "furniture_brand"             : cart.product.furniture.brand.name,
+                        "furniture_color_korean_name" : cart.product.color.korean_name,
+                        "furniture_color_english_name": cart.product.color.english_name,
+                        "price"                       : cart.product.price,
+                        "quantity"                    : cart.quantity,
+                    }
+                )
+            return JsonResponse({"carts" : results_cart}, status = 200)
+
+        return JsonResponse({'message':'fail'}, status=400)
