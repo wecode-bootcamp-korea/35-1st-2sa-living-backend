82bc91e6978c8dd0fbc3f5e472b47dc086ed64a4
melony <hjz1010@gmail.com>
2022-07-25 23:11:27 +0900

    MODIFY: Q 객체 사용방법 수정

--- products/views.py
+++ products/views.py
@@ -1,3 +1,6 @@
+import json
+from unicodedata import category
+
 from django.http           import JsonResponse
 from django.views          import View
 from django.core.paginator import Paginator
@@ -8,33 +11,33 @@ from .models import Category, SubCategory, Product
 
 class ProductListView(View):
     def get(self, request):
+
         category_id     = request.GET.get('category_id', None)
         sub_category_id = request.GET.get('sub_category_id', None)
         page_number     = request.GET.get('page', None)
 
+        q = Q()
+        if category_id:
+            q &= Q(sub_category__category_id=category_id)
+        if sub_category_id:
+            q &= Q(sub_category_id = sub_category_id) 
 
-        if int(category_id) in [c.id for c in Category.objects.all()] and int(sub_category_id) in [c.id for c in SubCategory.objects.all()]:
-            if SubCategory.objects.get(id=sub_category_id).category == Category.objects.get(id=category_id):
-                products = Product.objects.filter(Q(sub_category_id = sub_category_id) & Q(sub_category__category_id = category_id)) 
-                product_list = [{
-                    'id'         : product.id,
-                    'image'      : product.thumbnail_image_url,
-                    'brandName'  : product.furniture.brand.name,
-                    'productName': product.furniture.korean_name + '_' + product.color.korean_name,
-                    'price'      : product.price
-                } for product in products]    
+        products = Product.objects.filter(q) 
+        
+        product_list = [{
+            'id'         : product.id,
+            'image'      : product.thumbnail_image_url,
+            'brandName'  : product.furniture.brand.name,  
+            'productName': product.furniture.korean_name + '_' + product.color.korean_name,
+            'price'      : product.price
+        } for product in products]    
+        
+        paginator    = Paginator(product_list, 4)
+        product_list = paginator.page(page_number).object_list
+        page_list    = list(paginator.page_range)
 
-                try: 
-                    paginator    = Paginator(product_list, 4)
-                    product_list = paginator.page(page_number).object_list
-                    page_list    = list(paginator.page_range)
-                    return JsonResponse({'message': 'SUCCESS', 'product_list': product_list, 'page_list': page_list}, status=200)
-                except :
-                    return JsonResponse({'message': 'INVALID_PAGE'}, status=404)
-            else:
-                return JsonResponse({'message': 'DO_NOT_MATCH_CATEGORY'}, status=400)
-        else:
-            return JsonResponse({'message': 'INVALID_CATEGORY'}, status=404)
+        return JsonResponse({'message': 'SUCCESS', 'product_list': product_list, 'page_list': page_list}, status=200)
+
 
 
 class ProductDetailView(View):
@@ -43,7 +46,7 @@ class ProductDetailView(View):
             product          = Product.objects.get(id=product_id)
             related_products = Product.objects.filter(furniture_id=product.furniture_id)
 
-            result = [{
+            result = {
                     'english_name'         : product.furniture.english_name + '_' + product.color.english_name,
                     'korean_name'          : product.furniture.korean_name + '_' + product.color.korean_name,
                     'main_image'           : product.main_image_url,
@@ -53,7 +56,7 @@ class ProductDetailView(View):
                         'color': related_product.color.english_name,
                         'price': related_product.price
                     } for related_product in related_products]
-                }]
+                }
             return JsonResponse({'result': result}, status=200)
         except Product.DoesNotExist:
             return JsonResponse({'message': 'INVALID_PRODUCT_ID'}, status=404)
