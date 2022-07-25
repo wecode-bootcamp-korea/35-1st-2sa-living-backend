f6fca23657fd869762e2cb1d07a2fc16339f84ba
melony <hjz1010@gmail.com>
2022-07-25 17:48:38 +0900

    MODIFY: 수정요청 사항 반영 완료

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.http           import JsonResponse
 from django.views          import View
 from django.core.paginator import Paginator
+from django.db.models      import Q
 
 from .models import Category, SubCategory, Product
 
@@ -11,23 +12,28 @@ class ProductListView(View):
         sub_category_id = request.GET.get('sub_category_id', None)
         page_number     = request.GET.get('page', None)
 
-        if int(category_id) in [c.id for c in Category.objects.all()] and int(sub_category_id) in [c.id for c in SubCategory.objects.all()]:
-            products = Product.objects.filter(sub_category_id = sub_category_id) or Product.objects.filter(sub_category__category_id = category_id) 
-            product_list = [{
-                'id'         : product.id,
-                'image'      : product.thumbnail_image_url,
-                'brandName'  : product.furniture.brand.name,
-                'productName': product.furniture.korean_name + '_' + product.color.korean_name,
-                'price'      : product.price
-            } for product in products]    
 
-            try: 
-                paginator    = Paginator(product_list, 4)
-                product_list = paginator.page(page_number).object_list
-                page_list    = list(paginator.page_range)
-                return JsonResponse({'message': 'SUCCESS', 'product_list': product_list, 'page_list': page_list}, status=200)
-            except :
-                return JsonResponse({'message': 'INVALID_PAGE'}, status=400)
+        if int(category_id) in [c.id for c in Category.objects.all()] and int(sub_category_id) in [c.id for c in SubCategory.objects.all()]:
+            if SubCategory.objects.get(id=sub_category_id).category == Category.objects.get(id=category_id):
+                products = Product.objects.filter(Q(sub_category_id = sub_category_id) & Q(sub_category__category_id = category_id)) 
+                product_list = [{
+                    'id'         : product.id,
+                    'image'      : product.thumbnail_image_url,
+                    'brandName'  : product.furniture.brand.name,
+                    'productName': product.furniture.korean_name + '_' + product.color.korean_name,
+                    'price'      : product.price
+                } for product in products]    
+
+                try: 
+                    paginator    = Paginator(product_list, 4)
+                    product_list = paginator.page(page_number).object_list
+                    page_list    = list(paginator.page_range)
+                    return JsonResponse({'message': 'SUCCESS', 'product_list': product_list, 'page_list': page_list}, status=200)
+                except :
+                    return JsonResponse({'message': 'INVALID_PAGE'}, status=404)
+            else:
+                return JsonResponse({'message': 'DO_NOT_MATCH_CATEGORY'}, status=400)
         else:
-            return JsonResponse({'message': 'INVALID_CATEGORY'}, status=400)
-    
+            return JsonResponse({'message': 'INVALID_CATEGORY'}, status=404)
+
+
