4b9f093dff83b0e98e1b97e9080671c9f15ffca9
enddl3224 <enddl3224@naver.com>
2022-07-26 14:56:38 +0900

    MODIFY: carts APP의 views.py - DELETE 수정

--- carts/views.py
+++ carts/views.py
@@ -8,25 +8,24 @@ from carts.models    import Cart
 from core.utils      import LoginConfirm
 
 class CartView(View):
+
     @LoginConfirm
     def get(self, request):
+        
         carts = Cart.objects.filter(user_id = request.user.id)
-        results_cart = []
 
-        for cart in carts:
-            results_cart.append(
-                {
-                    "cart_id"                     : cart.id,
-                    "cart_image"                  : cart.product.thumbnail_image_url,
-                    "user_firstname"              : cart.user.first_name,
-                    "user_lastname"               : cart.user.last_name,
-                    "furniture_korean_name"       : cart.product.furniture.korean_name,
-                    "furniture_english_name"      : cart.product.furniture.english_name,
-                    "furniture_brand"             : cart.product.furniture.brand.name,
-                    "furniture_color_korean_name" : cart.product.color.korean_name,
-                    "furniture_color_english_name": cart.product.color.english_name,
-                    "price"                       : cart.product.price,
-                    "quantity"                    : cart.quantity,
-                }
-            )
+        results_cart = [{
+                "cart_id"                     : cart.id,
+                "cart_image"                  : cart.product.thumbnail_image_url,
+                "user_firstname"              : cart.user.first_name,
+                "user_lastname"               : cart.user.last_name,
+                "furniture_korean_name"       : cart.product.furniture.korean_name,
+                "furniture_english_name"      : cart.product.furniture.english_name,
+                "furniture_brand"             : cart.product.furniture.brand.name,
+                "furniture_color_korean_name" : cart.product.color.korean_name,
+                "furniture_color_english_name": cart.product.color.english_name,
+                "price"                       : cart.product.price,
+                "quantity"                    : cart.quantity,
+            }for cart in carts]
+
         return JsonResponse({"carts" : results_cart}, status = 200)
