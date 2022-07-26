ba82a2681ca82ae83117a918e9886826627af35d
강경훈 <kyunghun@gang-gyeonghuns-MacBook-Pro.local>
2022-07-26 11:14:44 +0900

    mentor review

--- products/views.py
+++ products/views.py
@@ -7,24 +7,65 @@ from .models import Category, SubCategory, Product
 
 
 class ProductListView(View):
+    """
+    목적: 제품의 목록을 반환
+
+    조건(필터)
+        - 카테고리 id로 필터
+        - 서브카테고리 id로 필터
+    조건(정렬)
+        - 신상품
+        - 추천
+        - 가격
+        - 판매
+    조건(페이지네이션)
+
+    구현 순선
+        1. 조건이 하나도 없는 제품 목록을 반환
+        2. 필터 조건을 1개씩만 구현
+        3. 만족한 조건들을 다 합쳐서 구현
+        4. 정렬
+        5. 페이지네이션
+
+    """
+
+    """
+    1. Request로 받은 값들 검증
+    2. 로직
+        - filter 조건만 조합
+        - 정렬 조건 확인
+        - 위에서 만든 조건으로 QuerySet을 가져옵니다. + 페이지네이션
+    3. Response
+    
+    """
+    
     def get(self, request):
+        #:8000/products?category_id=1&category_id=3
+        
         try:
             category_id     = request.GET.get('category_id', None)
             sub_category_id = request.GET.get('sub_category_id', None)
             page_number     = request.GET.get('page', 1)
-            
-            if category_id:
-                Category.objects.get(id = category_id)
-            if sub_category_id:
-                SubCategory.objects.get(id = sub_category_id)
-            
-            q = Q()
-            if category_id:
-                q &= Q(sub_category__category_id = category_id)
-            if sub_category_id:
-                q &= Q(sub_category_id = sub_category_id) 
+            sort            = request.GET.get('sort')
 
-            products = Product.objects.filter(q) 
+            q = Q()
+            
+            if category_id:
+                category = Category.objects.get(id = category_id)
+                q &= Q(sub_category__category = category)
+            
+            if sub_category_id:
+                sub_category = SubCategory.objects.get(id = sub_category_id)
+                q &= Q(sub_category = sub_category)
+
+            sort_set = {
+                "new"   : "created_at",
+                "price" : "price"
+            }
+
+            sort_field = sort_set.get(sort, "id") 
+
+            products = Product.objects.filter(q).order_by(sort_field)
             
             product_list = [{
                 'id'         : product.id,
@@ -43,7 +84,7 @@ class ProductListView(View):
             return JsonResponse({'message': 'INVALID_CATEGORY'}, status=404)
         except SubCategory.DoesNotExist:   
             return JsonResponse({'message': 'INVALID_SUBCATEGORY'}, status=404)        
-        except paginator.EmptyPage:
+        except Paginator.EmptyPage:
             return JsonResponse({'message': 'INVALID_PAGE'}, status=404)        
             
 
