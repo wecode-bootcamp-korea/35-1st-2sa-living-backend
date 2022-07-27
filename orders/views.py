13da9f838b942100c647b2e47dc3cdc7e5b05239
wj <wj@wjui-MacBookPro.local>
2022-07-27 16:40:57 +0900

    MODIFY : OrderView GET

--- orders/views.py
+++ orders/views.py
@@ -1,39 +1,36 @@
 import uuid
-from datetime		 import datetime
-from django.views    	 import View
-from django.http     	 import JsonResponse
-from django.db       	 import transaction
-from django.db.models	 import Sum,F
-from core.utils     	 import login_confirm
-from orders.models   	 import Order, OrderItem, OrderStatus
-from carts.models    	 import Cart
+
+from datetime         import datetime
+from django.views     import View
+from django.http      import JsonResponse
+from django.db        import transaction
+from django.db.models import Sum,F
+
+from core.utils       import login_confirm
+from orders.models    import Order, OrderStatus, OrderItem
+from carts.models     import Cart
+from users.models     import User
 
 class OrderView(View):
     @login_confirm
-    def get(self, request, *args, **kwargs):
+    def get(self, request):
         user        = request.user
-        orders      = Order.objects.filter(user=user)
-        order_items = OrderItem.objects.filter(order__in=orders)
-        total_price = order_items.aggregate(total_price=Sum(F('product__price')*F('quantity')))['total_price']
-        order_item_list = [{
-            'order_item_id'         : order_item.id,
-            'furniture_korean_name' : order_item.product.furniture.korean_name,
-            'furniture_english_name': order_item.product.furniture.english_name,
-            'product_image'         : order_item.product.thumbnail_image_url,
-            'product_korean_color'  : order_item.product.color.korean_name,
-            'product_english_color' : order_item.product.color.english_name,
-            'quantity'              : order_item.quantity,
-            'price'		    : int(order_item.product.price * order_item.quantity),
-            'created_at'            : order_item.order.created_at.strftime("%Y년 %m월 %d일 %H:%M"),
-            'updated_at'            : order_item.order.updated_at.strftime("%Y년 %m월 %d일 %H:%M"),
-        }for order_item in order_items]
+        order       = request.GET.get('order_id')
+        order_items = Order.objects.get(id=order).orderitem_set.all()
+        total_price = int(Order.objects.filter(id=order).annotate(total=Sum(F('orderitem__product__price')*F('orderitem__quantity')))[0].total)
+    
         order_list = [{
-            'order_id'       : order.id,
-            'order_number'   : order.order_number,
-            'user_first_name': user.first_name,
-            'user_last_name' : user.last_name,
-            'order_items'    : order_item_list,
-            'total_price'    : int(total_price),
-            'order_status'   : order.order_status.status,
-        }for order in orders]
-        return JsonResponse({"result" : order_list}, status=200)
+            'order_id'       : order,
+            'user_first_name': User.objects.get(id=user.id).first_name,
+            'user_last_name' : User.objects.get(id=user.id).last_name,
+            'order_items'    : [{
+                'product_id'           : order_item.product.id,
+                'quantity'             : order_item.quantity,
+                'furniture_korean_name': order_item.product.furniture.korean_name,
+                'product_image'        : order_item.product.thumbnail_image_url,
+                'price'                : int((order_item.quantity) * (order_item.product.price))
+            } for order_item in order_items],
+            'total_price' : total_price
+        }]
+        
+        return JsonResponse({'order_list':order_list}, status=200)
