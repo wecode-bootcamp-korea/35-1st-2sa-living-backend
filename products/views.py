a85b5c0561cf2d51087c365ae5fa8fcb97a8e875
melony <hjz1010@gmail.com>
2022-07-27 09:37:51 +0900

    이 브랜치에 해당하지 않는 변경사항 삭제

--- products/views.py
+++ products/views.py
@@ -65,7 +65,7 @@ class ProductDetailView(View):
             product          = Product.objects.get(id=product_id)
             related_products = Product.objects.filter(furniture_id=product.furniture_id)
 
-            result = {
+            result = [{
                     'english_name'         : product.furniture.english_name + '_' + product.color.english_name,
                     'korean_name'          : product.furniture.korean_name + '_' + product.color.korean_name,
                     'main_image'           : product.main_image_url,
@@ -75,7 +75,7 @@ class ProductDetailView(View):
                         'color': related_product.color.english_name,
                         'price': related_product.price
                     } for related_product in related_products]
-                }
+                }]
             return JsonResponse({'result': result}, status=200)
         except Product.DoesNotExist:
             return JsonResponse({'message': 'INVALID_PRODUCT_ID'}, status=404)
