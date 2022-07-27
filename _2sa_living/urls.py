302bce9f23325d34e0bd2c05c51e22df05f9202a
wj <wj@wjui-MacBookPro.local>
2022-07-27 17:42:16 +0900

    fix_conflict

--- _2sa_living/urls.py
+++ _2sa_living/urls.py
@@ -13,13 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.urls import path, include
 
 urlpatterns = [
     path('users', include('users.urls')),
     path('carts', include('carts.urls')),
-    path('orders', include('orders.urls')),
     path('products', include('products.urls')),
-    
+    path('orders', include('orders.urls'))
 ]
+
