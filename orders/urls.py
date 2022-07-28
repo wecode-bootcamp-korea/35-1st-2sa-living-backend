69176ebb613851d7eacd89736fb8ffd45f274787
enddl3224 <enddl3224@naver.com>
2022-07-28 18:03:10 +0900

    MODIFY: ordersAPP의 CartListView 수정

--- orders/urls.py
+++ orders/urls.py
@@ -3,4 +3,5 @@ from orders.views import OrderView, OrderListView
 
 urlpatterns = [
     path('/<int:order_id>', OrderView.as_view()),
+    path('/list', OrderListView.as_view()),
 ]
--- orders/views.py
+++ orders/views.py
@@ -40,3 +40,24 @@ class OrderView(View):
         }]
         
         return JsonResponse({'order_list':order_list}, status=200)
+
+class OrderListView(View):
+    @login_confirm
+    def get(self, request):
+        user   = request.user
+        orders = Order.objects\
+                      .annotate(total_price=Sum(F('orderitem__product__price')*F('orderitem__quantity')))\
+                      .filter(user=user)
+        
+        order_list=[
+            {
+                "order_id"     : order.id,
+                "order_number" : order.order_number,
+                "order_status" : order.order_status.status,
+                "total_price"  : order.total_price,
+                'created_at'   : order.created_at.strftime("%Y년 %m월 %d일 %H:%M"),
+                'updated_at'   : order.updated_at.strftime("%Y년 %m월 %d일 %H:%M"),
+
+            } for order in orders
+        ]
+        return JsonResponse({"result": order_list}, status=200)
