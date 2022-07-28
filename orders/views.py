1105e7549d09a1b7c61317b512cc1aed719ae590
강경훈 <kyunghun@gang-gyeonghuns-MacBook-Pro.local>
2022-07-28 14:42:22 +0900

    mentor review

--- orders/views.py
+++ orders/views.py
@@ -9,22 +9,23 @@ from django.db.models import Sum,F
 from core.utils       import login_confirm
 from orders.models    import Order, OrderStatus, OrderItem
 from carts.models     import Cart
-from users.models     import User
 
 class OrderListView(View):
     @login_confirm
     def get(self, request):
         user   = request.user
-        orders = Order.objects.filter(user_id = user.id)
+        orders = Order.objects\
+                      .annotate(total_price=Sum(F('product__price')*F('quantity')))\
+                      .filter(user=user)
         
         order_list=[
             {
-                "order_id"    : order.id,
-                "order_number": order.order_number,
-                "order_status": order.order_status.status,
-                "total_price" : OrderItem.objects.filter(order=order).aggregate(total=Sum(F('product__price')*F('quantity')))['total'],
-                'created_at'  : order.created_at.strftime("%Y년 %m월 %d일 %H:%M"),
-                'updated_at'  : order.updated_at.strftime("%Y년 %m월 %d일 %H:%M"),
+                "order_id"     : order.id,
+                "order_number" : order.order_number,
+                "order_status" : order.order_status.status,
+                "total_price"  : order.total_price,
+                'created_at'   : order.created_at.strftime("%Y년 %m월 %d일 %H:%M"),
+                'updated_at'   : order.updated_at.strftime("%Y년 %m월 %d일 %H:%M"),
 
             } for order in orders
         ]
